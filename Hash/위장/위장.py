40cbd834ac471281f837c4df9ce52fdcfd262ea4
Wontae <wontae.choi@mail.utoronto.ca>
2019-03-05 12:04:27 -0500

    위장 완료

---
+++ Hash/위장/위장.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    clothes_hash = {}
+    for c in clothes:
+        if c[1] not in clothes_hash:
+            clothes_hash[c[1]] = []
+        clothes_hash[c[1]].append(c[0])
+    total = 1
+    
+    for c in clothes_hash:
+        total *= (len(clothes_hash[c])+1)
+        
+    answer = total - 1
+    return answer
